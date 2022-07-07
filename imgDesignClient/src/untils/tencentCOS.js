9c961c3506831b40f7c31c29c0db798db96fdaa2
Mintimate <mintimate215@gmail.com>
2022-07-07 20:07:48 +0800

    Update tencentCOS.js

--- imgDesignClient/src/untils/tencentCOS.js
+++ imgDesignClient/src/untils/tencentCOS.js
@@ -1,14 +1,14 @@
 import COS from "cos-js-sdk-v5"
 
-const bucket = 'image-file-host-1302972711';
+const bucket = 'image-file';
 const region = 'ap-shanghai';
-const prefix = 'https://image-file-host-1302972711.cos.ap-shanghai.myqcloud.com/img2dPost/'
+const prefix = ''
 
 export function uploadFile(file, fileName) {
 
   let cos = new COS({
-    SecretId: 'AKIDPZSxBI2h281fNtTXVEguswlZ8PRC7Zv1',
-    SecretKey: 'KODl4TbU8YjicGnNhxykHWXIFuMkyYwp',
+    SecretId: '',
+    SecretKey: '',
     ProgressInterval: 1000 // 控制上传的 onProgress 回调的间隔
   });
   cos.putObject({
